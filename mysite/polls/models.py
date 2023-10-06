7f58e814dc68a211ed38e2a36eda6b71da61f7a2
Floysh3 <floysh3@mail.ru>
2023-10-06 19:20:20 +0300

    add migrations

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class MyUser(User):
-    first_name = models.CharField(max_length=30)
-    second_name = models.CharField(max_length=30)
     email_address = models.EmailField(max_length=50)
     registration_date = models.DateField(auto_now=True)
     likes = models.ForeignKey("Like", on_delete=models.CASCADE, null=True)
@@ -13,7 +11,7 @@ class MyUser(User):
 class PicturePost(models.Model):
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
     likes = models.ForeignKey("Like", on_delete=models.CASCADE, null=True)
-    comments = models.ForeignKey("Comment", on_delete=models.CASCADE)
+    comments = models.ForeignKey("Comment", on_delete=models.CASCADE, null=True)
     pub_date = models.DateTimeField(auto_now=True)
     description = models.TextField(max_length=300)
     name = models.CharField(max_length=50)
@@ -24,13 +22,13 @@ class PicturePost(models.Model):
 class Comment(models.Model):
     pub_date = models.DateTimeField(auto_now=True)
     content = models.TextField(max_length=300)
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    post = models.ForeignKey(PicturePost, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(PicturePost, on_delete=models.CASCADE, null=True)
 
 
 class Like(models.Model):
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    post = models.ForeignKey(PicturePost, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(PicturePost, on_delete=models.CASCADE, null=True)
 
 
 
