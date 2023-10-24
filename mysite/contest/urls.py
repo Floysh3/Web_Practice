2ecdd1521d1997ee02b47b39eed8253f8789ff2a
Floysh3 <floysh3@mail.ru>
2023-10-24 20:28:55 +0300

    add templ

--- mysite/contest/urls.py
+++ mysite/contest/urls.py
@@ -5,7 +5,7 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('user_profile/', views.user_profile, name='user_profile'),
     path('photo/', views.photo, name='photo'),
-
-
+    path('authorisation/', views.authorisation, name='authorisation'),
+    path('aregistration/', views.registration, name='registration'),
 
 ]
--- mysite/contest/views.py
+++ mysite/contest/views.py
@@ -6,14 +6,25 @@ from .models import *
 from django.http import HttpResponseNotFound
 # Create your views here.
 
+menu = [{'title':"Войти", 'url_name':'authorisation'},{'title':"Регистрация", 'url_name':'registration'}]
+
 def index(request):
     posts = PicturePost.objects.all()
-    return render(request, 'index.html', {'posts': posts, 'title':'Главная страница'})
+    return render(request, 'index.html', {'menu': menu, 'posts':posts, 'title':'Главная страница'})
 
 def user_profile(request):
     template = loader.get_template('user_profile.html')
-    return HttpResponse(template.render())
+    return render(request, 'user_profile.html', {'menu': menu, 'title':'Страница пользователя'})
 
 def photo(request):
     template = loader.get_template('photo.html')
     return HttpResponse(template.render())
+
+def authorisation(request):
+    template = loader.get_template('authorisation.html')
+    return render(request, 'authorisation.html', {'menu': menu, 'title':'Страница авторизации'})
+
+def registration(request):
+    template = loader.get_template('registration.html')
+    return render(request, 'registration.html', {'menu': menu, 'title':'Страница регистрации'})
+
